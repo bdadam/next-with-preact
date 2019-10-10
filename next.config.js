d1f9353fb1f6184f837848ee2adbacede66157fa
Adam <me@bdadam.com>
2019-10-10 15:25:45 +0200

    Simple next app

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    webpack: (config, options) => {
+        config.resolve.alias = Object.assign({}, config.resolve.alias, {
+            react: 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react-dom': 'preact/compat',
+        });
+
+        return config;
+    },
+    options: {
+        outDir: 'dist',
+    },
+};
